d3d893a2bcacc7f85de67999cfae2a3568ad0ea0
Stephen Rozo <rozo.stephenb@gmail.com>
2017-12-12 17:54:29 -0500

    Update Interface.js

--- js/Interface.js
+++ js/Interface.js
@@ -104,17 +104,15 @@ Interface.Dragger = function(params){
 		this.element = $("<div>", {
 			"class" : "Dragger",
 			"id" : name
- +
-  			}).appendTo(this.container)
-  				.on("dragMove", this._ondrag.bind(this))
-  				.on("touchstart mousedown", this._onstart.bind(this))
-  				.on("dragEnd touchend mouseup", this._onend.bind(this));
-  
- -			this.name = $("<div>", {
- -				"id" : "Name",
- -				"text" : name
- -			}).appendTo(this.element);
-		
+		}).appendTo(this.container)
+			.on("dragMove", this._ondrag.bind(this))
+			.on("touchstart mousedown", this._onstart.bind(this))
+			.on("dragEnd touchend mouseup", this._onend.bind(this));
+
+		this.name = $("<div>", {
+			"id" : "Name",
+			"text" : name
+		}).appendTo(this.element);
 
 		this.element.draggabilly({
 			"axis" : this.axis,
